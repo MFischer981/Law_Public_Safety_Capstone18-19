dbeb97eab820ff99b5f3ca85fa826c0cfbcd5b2c
Micah Fischer <mfischer981@west-mec.org>
2019-05-02 15:23:30 -0700

    Created custom form validation
    Created custom form validation for the contact form. Added clear and submit buttons for the form.

--- main.js
+++ main.js
@@ -2,3 +2,56 @@ function menuOpen() {
     document.getElementById("menu").classList.toggle("open");
     document.getElementById("wrapper").classList.toggle("menu");
 }
+
+function runSubmit() {
+    validateName();
+    validateCompany();
+    validatePhone();
+    validateEmail();
+    validateComment();
+}
+
+function validateName() {
+    var formName = document.getElementById("name");
+    if (formName.validity.valueMissing) {
+        formName.setCustomValidity("Please enter your name.")
+    } else {
+        formName.setCustomValidity("");
+    }
+}
+
+function validateCompany() {
+    var formCompany = document.getElementById("company");
+    if (formCompany.validity.valueMissing) {
+        formCompany.setCustomValidity("Please enter your company's name.")
+    } else {
+        formCompany.setCustomValidity("");
+    }
+}
+
+function validatePhone() {
+    var formPhone = document.getElementById("phone");
+    if (formPhone.validity.valueMissing) {
+        formPhone.setCustomValidity("Please enter your phone number.")
+    } else {
+        formPhone.setCustomValidity("");
+    }
+}
+
+function validateEmail() {
+    var formPhone = document.getElementById("email");
+    if (formPhone.validity.valueMissing) {
+        formPhone.setCustomValidity("Please enter your email address.")
+    } else {
+        formPhone.setCustomValidity("");
+    }
+}
+
+function validateComment() {
+    var formComment = document.getElementById("comment");
+    if (formComment.validity.valueMissing) {
+        formComment.setCustomValidity("Please enter a comment.")
+    } else {
+        formComment.setCustomValidity("");
+    }
+}
