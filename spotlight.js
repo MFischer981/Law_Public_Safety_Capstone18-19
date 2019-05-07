e094a6e5d63cb268216e78293a6187142dd476f2
Micah Fischer <mfischer981@west-mec.org>
2019-05-07 15:26:52 -0700

    Added arrays and click events to game events
    Added arrays and click events to the game. Added descriptions for evidence in game. Added titles and pictures on click events. Fixed issue with object layering.

--- spotlight.js
+++ spotlight.js
@@ -29,3 +29,14 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+var titles = ["Evidence Marker", "Drugs", "Fingerprints", "Documents", "Footprints", "Blood Spatter", "Victim's Body", "Murder Weapon: Firearm"];
+
+var explanations = ["Evidence Markers are use to document the evidence at a crime scene. This is especially crucial at large crime scenes where there is lots of evidence to process.", "This could have been a drug deal gone wrong. We will need to identify the pills in this bottle back at the forensics lab.", "Fingerprints are specific too each and every person on earth. We can use fingerprints to match a suspect to place them at the crime scene.", "We don't know the contents of these documents but they seem to be important. We will have an investigator review them later.", "This blood spatter indicates the victim sustained some sort of terminal injury. We will have to preform an autopsy to confirm the cause of death.", "Using the victims ID we can form connections to people who may have been involved in the victim's murder or have important information."]
+
+var photos = ["<img src='marker1.png'>", "<img src='drugs.png'>", "<img src='prints.png'>", "<img src='documents.png'>", "<img src='footprints.png'>", "<img src='blood.png'>", "<img src='body.png'>", "<img src='weapon.png'>"]
+
+function popUp(n) {
+    document.getElementById("evidence").innerHTML = titles[n];
+    // document.getElementById("").innerHTML = explanations;
+    // document.getElementById("").innerHTML = photos;
+}
