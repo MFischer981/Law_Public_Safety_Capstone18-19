49537962f93fc77989d0eeb9c4a30da71bd6e272
Unknown <micahefish@gmail.com>
2019-05-08 06:55:47 -0700

    Add pop up element on click for interactive elements.
    Updated crime scene spotlight and click system. Added time interval delay countdown for enrollment countdown. Added section for applications.

--- main.js
+++ main.js
@@ -55,3 +55,32 @@ function validateComment() {
         formComment.setCustomValidity("");
     }
 }
+
+
+var countDownDate = new Date("Aug 1, 2019 0:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+    
+
+  var distance = countDownDate - now;
+    
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+  document.getElementById("time").innerHTML = days + " Days " + hours + " Hours "
+  + minutes + " Minutes " + seconds + " Seconds ";
+    
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "WestMEC has Started!";
+  }
+}, 1000);
--- spotlight.js
+++ spotlight.js
@@ -2,13 +2,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const spotlight = document.querySelector('.spotlight');
 
-    let spotlightSize = 'transparent 80px, rgba(0, 0, 0, 0.95) 100px)';
+    let spotlightSize = 'transparent 120px, rgba(0, 0, 0, 0.75) 160px)';
 
     window.addEventListener('mousemove', e => updateSpotlight(e));
 
     window.addEventListener('mousedown', e => {
 
-        spotlightSize = 'transparent 60px, rgba(0, 0, 0, 0.95) 80px)';
+        spotlightSize = 'transparent 120px, rgba(0, 0, 0, 0.75) 160px)';
 
         updateSpotlight(e);
 
@@ -16,7 +16,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener('mouseup', e => {
 
-        spotlightSize = 'transparent 80px, rgba(0, 0, 0, 0.95) 100px)';
+        spotlightSize = 'transparent 120px, rgba(0, 0, 0, 0.75) 160px)';
 
         updateSpotlight(e);
 
@@ -29,14 +29,39 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-var titles = ["Evidence Marker", "Drugs", "Fingerprints", "Documents", "Footprints", "Blood Spatter", "Victim's Body", "Murder Weapon: Firearm"];
+var titles = ["Evidence Marker", "Drugs", "Fingerprints", "Document", "Footprints", "Blood Spatter", "Victim's Body", "Murder Weapon: <br> Beretta 8000", "Wallet"];
 
-var explanations = ["Evidence Markers are use to document the evidence at a crime scene. This is especially crucial at large crime scenes where there is lots of evidence to process.", "This could have been a drug deal gone wrong. We will need to identify the pills in this bottle back at the forensics lab.", "Fingerprints are specific too each and every person on earth. We can use fingerprints to match a suspect to place them at the crime scene.", "We don't know the contents of these documents but they seem to be important. We will have an investigator review them later.", "This blood spatter indicates the victim sustained some sort of terminal injury. We will have to preform an autopsy to confirm the cause of death.", "Using the victims ID we can form connections to people who may have been involved in the victim's murder or have important information."]
+var desc = [
+// Evidence Markers
+"Evidence Markers are use to document the evidence at a crime scene. This is especially crucial at large crime scenes where there is lots of evidence to process.", 
+// Drugs
+"This could have been a drug deal gone wrong. We will need to identify the pills in this bottle back at the forensics lab.", 
+// Fingerprints
+"Fingerprints are specific to each and every person on earth. We can use fingerprints to match a suspect to place them at the crime scene.",
+// Document 
+"This small sheet of notepad contains a phone number attached to a name. We cam run the name and number through our database to help track down a possible witness or suspect.",
+// Shoe Print 
+"This shoe print could help us identify the footwear that a suspect or witness was wearing. We will have to run the footprint against our database.",
+// Blood Spatter 
+"This blood spatter indicates the victim sustained some sort of terminal injury. We will have to preform an autopsy to confirm the cause of death.",
+// Victim IDs
+"Using the victims ID we can form connections to people who may have been involved in the victim's murder or have important information.", 
+// Handgun
+"This is a possible murder weapon. We will have to run a ballistics test on the gun to determine wether or not it is a murder weapon",
+// Wallet
+"The victim's wallet is still at the crime scene which proves to us this was not a mugging or robbery. Also whoever our victim is he is carrying way to much cash."]
 
-var photos = ["<img src='marker1.png'>", "<img src='drugs.png'>", "<img src='prints.png'>", "<img src='documents.png'>", "<img src='footprints.png'>", "<img src='blood.png'>", "<img src='body.png'>", "<img src='weapon.png'>"]
+var photos = ["<img id='picture' src='marker.png'>", "<img id='picture' src='drugs.png'>", "<img id='picture' src='prints.png'>", "<img id='picture' src='documents.png'>", "<img id='picture' src='footprint.png'>", "<img id='picture' src='blood.png'>", "<img id='picture' src='body.png'>", "<img id='picture' src='weapon.png'>", "<img id='picture' src='wallet.png'>"]
 
 function popUp(n) {
+    document.getElementById("popup").style.display = "block";
     document.getElementById("evidence").innerHTML = titles[n];
-    // document.getElementById("").innerHTML = explanations;
-    // document.getElementById("").innerHTML = photos;
+    document.getElementById("description").innerHTML = desc[n];
+    document.getElementById("picture").outerHTML = photos[n];
 }
+
+function closeWindow() {
+    console.log("close");
+    document.getElementById("popup").style.display = "none";
+}
+
